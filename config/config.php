9de06108d4cce402417d9282ad44f4f702e86d54
Davicho27 <87341245+Davicho27@users.noreply.github.com>
2024-08-04 15:14:35 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 	/*===========================================
 	|  Datos del servidor - Data of the server  |
 	===========================================*/
-	const SERVER="http://34.145.211.74/";
+	const SERVER="34.145.211.74";
 	const DB="retroaula";
 	const USER='root';
 	const PASS="e707f321-5c90-4fb9-a586-1cf2943ed6e6";
